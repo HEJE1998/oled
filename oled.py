d635595ec709466331c5ca96a3c1c701d544f354
HEJE1998 <JeldrikHemme98@web.dem>
2021-11-22 17:02:16 +0100

    Version 3: Kommentare alle eingefügt

--- oled.py
+++ oled.py
@@ -4,7 +4,7 @@
 
 #Hardware: ESP 32 + BMP 180
 
-#Version: 0.2
+#Version: 0.3
 
 from time import sleep                          #Von time den Befehl sleep importieren 
 from machine import Pin, SoftSPI, SoftI2C       #Von machine die Klasse Pin, SoftSPI, SoftI2C importieren 
@@ -19,25 +19,26 @@ from romfonts import vga2_16x16 as font         #Von romfonts die Klasse (Schrif
 ic2 = SoftI2C(scl=Pin(22), sda=Pin(21))         #Schnittstellen für den I2C-Bus sind scl=Pin22 und sda=Pin21
 bmp = BMP180(i2c)                               #Variable bmp wird über den I2C-Bus des Klasse BMP180 bestimmt 
 
+#Konfiguration des SPI-Protokolls 
 spi = SoftSPI(
-        baudrate=20000000,
-        polarity=1,
-        phase=0,
-        sck=Pin(18),
-        mosi=Pin(19),
-        miso=Pin(13))
+        baudrate=20000000,                      #defeniert die Geschwindigkeit der Kommunikation        
+        polarity=1,                             #kann 0 oder 1 sein und geibt den Pegel an auf, welchem sich die Leerlauftaktleistung sich befindet 
+        phase=0,                                #kann 0 oder 1 sein, bei 1 überprüft sie Daten an der ersten oder zweiten Taktflanke 
+        sck=Pin(18),                            #hier wird festegelegt das der 18.Pin vom Controller der Pin für sck ist 
+        mosi=Pin(19),                           #hier wird festegelegt das der 19.Pin vom Controller der Pin für mosi ist 
+        miso=Pin(13))                           #hier wird festegelegt das der 13.Pin vom Controller der Pin für miso ist 
 
 tft = st7789.ST7789(
-       spi,
-       135,
-       240,
-       reset=Pin(23, Pin.OUT),
-       cs=Pin(5, Pin.OUT),
-       dc=Pin(16, Pin.OUT),
-       backlight=Pin(4, Pin.OUT),
-       rotation=1)
+       spi,                                     #Angabe des Kommunikations-Protokoll 
+       135,                                     #Angabe der Breite des Displays 
+       240,                                     #Angabe der Länge des Displays 
+       reset=Pin(23, Pin.OUT),                  #hier wird festgelegt das der 23.Pin vom Controller der Pin für reset ist 
+       cs=Pin(5, Pin.OUT),                      #hier wird festegelegt das der 5.Pin vom Controller der Pin für cs ist 
+       dc=Pin(16, Pin.OUT),                     #hier wird festegelegt das der 16.Pin vom Controller der Pin für dc ist 
+       backlight=Pin(4, Pin.OUT),               #hier wird festegelegt das der 4.pin vom Controller der Pin für backlight ist  
+       rotation=1)                              #Darstellungseinstellung (1=Protrait) 
 
-tft.fill(st7789.BLACK)                  #tft.fill, sagt aus welche Farbe der Hintergrund haben soll 
+tft.fill(st7789.BLACK)                  #tft.fill, sagt aus welche Farbe der gemsate Hintergrund haben soll 
 line = 0
 col = 0
 while True:
