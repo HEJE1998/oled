2f5c6add519093597b0bf53588322deef4cdcf40
HEJE1998 <JeldrikHemme98@web.dem>
2021-11-23 14:33:27 +0100

    Version 5: LED-Anzeige nach Temperatur

--- oled.py
+++ oled.py
@@ -4,7 +4,7 @@
 
 #Hardware: ESP 32 + BMP 180
 
-#Version: 0.4
+#Version: 0.5
 
 from time import sleep                          # alle 10 Sekunden Temperatur messen 
 from machine import Pin, SoftSPI, SoftI2C       # Pin (BMP180 & TFT), SoftSPI (TFT), SoftI2C(BMP180) 
@@ -34,15 +34,43 @@ tft = st7789.ST7789(                            # Objekt (TFT) istanzieren
        backlight=Pin(4, Pin.OUT),               #  
        rotation=1)                              # Rotation 1=90 Grad 
 
+ledgrün = Pin(2, Pin.OUT)                       #
+ledgelb = Pin(27, Pin.OUT)                      #
+ledrot = Pin(26, Pin.OUT)                       #
+
 #-------------Initialisierungs Ende--------------
 
 tft.fill(st7789.BLACK)                                                  # Hintergrundfarbe Schwarz
 line = 0                                                                # Zeile
 col = 0                                                                 # Spalte
-while True:                                                             # 
-    ausgabe = str(round(bmp.temperature, 2))                            # ausgabe zuweisen
-    ausgabe1= str(round(bmp.pressure / 100, 2))                         # ausgabe1 zuweisen
-    ausgabe = ausgabe + '\xf8C'                                         # Einheit hinzugefügt
-    ausgabe1 = ausgabe1 + str('hPascal')                                # Einheit hinzugefügt 
-    tft.text(font, ausgabe, 0, 20, st7789.YELLOW, st7789.BLACK)            
-    tft.text(font, ausgabe1, 0, 40, st7789.YELLOW, st7789.BLACK)       
+while True:
+        ausgabe = str(round(bmp.temperature, 2))                            # ausgabe zuweisen
+        ausgabe1= str(round(bmp.pressure / 100, 2))                         # ausgabe1 zuweisen
+        ausgabe = ausgabe + '\xf8C'                                         # Einheit hinzugefügt
+        ausgabe1 = ausgabe1 + str('hPascal')                                # Einheit hinzugefügt 
+        tft.text(font, ausgabe, 0, 20, st7789.WHITE, st7789.BLACK)            
+        tft.text(font, ausgabe1, 0, 40, st7789.WHITE, st7789.BLACK)
+
+        temperatur = bmp.temperature                                        # temperatur zuweisen     
+        if temperatur <= 23:                                                # LED nach Temperaturanzeige 
+                ledgrün.value(1)                                            
+                ledgelb.value(0)
+                ledrot.value(0) 
+        elif temperatur > 23 and temperatur <= 24:                          
+                ledgrün.value(0)
+                ledgelb.value(1)
+                ledrot.value(0)
+        elif temperatur > 24:
+                ledgrün.value(0)
+                ledgelb.value(0)
+                ledrot.value(1)
+        sleep(1)                                                             # Warten 5s
+
+
+
+
+            
+        
+
+
+
